8f9f537ad16c1b8b06db61d2dad3b09e37b46b2a
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-02-13 17:54:10 -0300

    Funções que incluem elementos na lista foram adicionadas

--- Listas/Lista_Estatica/tad_lista.c
+++ Listas/Lista_Estatica/tad_lista.c
@@ -43,3 +43,43 @@ int TAD_VerificarListaVazia(Lista *lista)
 	if(lista == NULL) return TAD_LISTA_INEXISTENTE;
 	return lista->elementos == TAD_LISTA_INICIO;
 }
+
+int TAD_IncluirElementoInicio(Lista *lista, Cliente cliente)
+{
+	if(lista == NULL) return TAD_LISTA_INEXISTENTE;
+	if(TAD_VerificarListaCheia(lista)) return TAD_LISTA_CHEIA;
+	for(int i = lista->elementos-1; i >= TAD_LISTA_INICIO; i--)
+	{
+		lista->cliente[i+1] = lista->cliente[i];
+	}
+	lista->cliente[TAD_LISTA_INICIO] = cliente;
+	lista->elementos++;
+	return TAD_LISTA_SUCESSO;
+}
+
+int TAD_IncluirElementoFinal(Lista *lista, Cliente cliente)
+{
+	if(lista == NULL) return TAD_LISTA_INEXISTENTE;
+	if(TAD_VerificarListaCheia(lista)) return TAD_LISTA_CHEIA;
+	lista->cliente[lista->elementos] = cliente;
+	lista->elementos++;
+	return TAD_LISTA_SUCESSO;
+}
+
+int TAD_IncluirElementoOrdenada(Lista *lista, Cliente cliente)
+{
+	if(lista == NULL) return TAD_LISTA_INEXISTENTE;
+	if(TAD_VerificarListaCheia(lista)) return TAD_LISTA_CHEIA;
+	int k, i = TAD_LISTA_INICIO;
+	while(i < lista->elementos && lista->cliente[i].codigo < cliente.codigo)
+	{
+		i++
+	}
+	for(k = lista->elementos-1; k >= i; k--)
+	{
+		lista->cliente[k+1] = lista->cliente[k];
+	}
+	lista->cliente[i] = cliente;
+	lista->elementos++;
+	return TAD_LISTA_SUCESSO;
+}
--- Listas/Lista_Estatica/tad_lista.h
+++ Listas/Lista_Estatica/tad_lista.h
@@ -7,6 +7,7 @@
 //	Erros
 #define TAD_LISTA_SUCESSO 0
 #define TAD_LISTA_INEXISTENTE -1
+#define TAD_LISTA_CHEIA -2
 
 /*	Estruturas */
 struct tipo_cliente
@@ -25,5 +26,8 @@ int TAD_LiberarLista(Lista *lista);
 int TAD_VerificarListaTamanho(Lista *lista);
 int TAD_VerificarListaCheia(Lista *lista);
 int TAD_VerificarListaVazia(Lista *lista);
+int TAD_IncluirElementoInicio(Lista *lista, Cliente cliente);
+int TAD_IncluirElementoFinal(Lista *lista, Cliente cliente);
+int TAD_IncluirElementoOrdenada(Lista *lista, Cliente cliente);
 
 #endif
