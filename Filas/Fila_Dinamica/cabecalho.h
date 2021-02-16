f56cadd5642422d887ea6825dd61ecc6c98a667f
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-02-16 20:09:40 -0300

    Arquivos basicos do programa principal foram adicionados

---
+++ Filas/Fila_Dinamica/cabecalho.h
@@ -0,0 +1,13 @@
+#ifndef CABECALHO_H_INCLUDED
+#define CABECALHO_H_INCLDUED
+
+#include "tad_fila.h"
+
+/*  Constantes */
+
+/*  Typedef */
+typedef Elemento Pacote;
+
+/*  Funcoes */
+
+#endif
---
+++ Filas/Fila_Dinamica/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cabecalho.h"
+
+int main(void)
+{
+    //  Code
+
+    return 0;
+}
