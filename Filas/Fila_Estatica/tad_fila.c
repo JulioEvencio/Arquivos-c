97b03e0343044639939ba94cd0962166df964f75
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-02-16 17:27:22 -0300

    Arquivos básicos da fila foram adicionados

---
+++ Filas/Fila_Estatica/tad_fila.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "tad_fila.h"
+
+/*  Estruturas */
+
+/*  Funcoes */
---
+++ Filas/Fila_Estatica/tad_fila.h
@@ -0,0 +1,12 @@
+#ifndef TAD_FILA_H_INCLUDED
+#define TAD_FILA_H_INCLUDED
+
+/*  Constantes */
+
+/*  Estruturas */
+
+/*  Ponteiros */
+
+/*  Funcoes */
+
+#endif
