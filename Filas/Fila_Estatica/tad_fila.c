ea7bbd43a6d8515ab26f31487c3fba71f76bc845
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-02-16 17:41:37 -0300

    Constantes e tipo da lista/elemento foram adicionados

--- Filas/Fila_Estatica/tad_fila.c
+++ Filas/Fila_Estatica/tad_fila.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include "tad_fila.h"
 
+/*  Constantes */
+#define _TAD_LISTA_INICIO 0
+#define _TAD_LISTA_FINAL 0
+#define _TAD_LISTA_TAMANHO_INICIO 0
+
 /*  Estruturas */
+struct tipo_fila
+{
+    int tamanho;
+    int inicio;
+    int final;
+    Elemento elemento[TAD_FILA_TAMANHO_MAX];
+};
 
 /*  Funcoes */
--- Filas/Fila_Estatica/tad_fila.h
+++ Filas/Fila_Estatica/tad_fila.h
@@ -2,10 +2,25 @@
 #define TAD_FILA_H_INCLUDED
 
 /*  Constantes */
+#define NOME_TAMANHO 80
+#define TAD_FILA_TAMANHO_MAX 100
+//	Erros
+#define TAD_FILA_SUCESSO 0
+#define TAD_FILA_INEXISTENTE -1
+#define TAD_FILA_CHEIA -2
+#define TAD_FILA_VAZIA -3
+#define TAD_ELEMENTO_INEXISTENTE -4
+#define TAD_POSICAO_INVALIDA -5
 
 /*  Estruturas */
+struct tipo_elemento
+{
+    int n;
+};
 
-/*  Ponteiros */
+/*  Typedef */
+typedef struct tipo_fila Fila;
+typedef struct tipo_elemento Elemento;
 
 /*  Funcoes */
 
