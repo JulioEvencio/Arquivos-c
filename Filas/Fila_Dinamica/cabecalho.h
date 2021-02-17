670f18f6dcb1a664b5daeee6d0d1830a88b14fb1
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-02-17 20:21:42 -0300

    Finalizado / Falta melhorar código

--- Filas/Fila_Dinamica/cabecalho.h
+++ Filas/Fila_Dinamica/cabecalho.h
@@ -4,12 +4,23 @@
 #include "tad_fila.h"
 
 /*  Constantes */
+#define NUMERO_FILA 4
+#define NUMERO_PACOTES 10
+#define NUMERO_CLIENTES 3
+#define NOME_PACOTE "pkt"
 #define LIMPAR_TELA "clear"
+#define FILA_PROCESSADOR 3
 
 /*  Typedef */
 typedef Elemento Pacote;
 
 /*  Funcoes */
 void delay(int delay);
+void processar_similacao(Fila **fila);
+void criar_pacote(Pacote *pacote);
+void colocar_fila(Fila *fila);
+void enviar_processador(Fila *fila_cliente, Fila *fila_processador);
+void remover_processador(Fila *fila_processador);
+void montar_tela(Fila **fila);
 
 #endif
--- Filas/Fila_Dinamica/main.c
+++ Filas/Fila_Dinamica/main.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "cabecalho.h"
 
 int main(void)
 {
-    //  Code
+    int loop = 1;
+    Fila *fila[NUMERO_FILA];
+
+    srand(time(NULL));
+
+    for(int i = 0; i < NUMERO_FILA; i++)
+    {
+        fila[i] = TAD_CriarFila();
+        if(fila[i] == NULL)
+        {
+            puts("Erro ao criar fila");
+            return 1;
+        }
+    }
+
+    while(loop)
+    {
+        system(LIMPAR_TELA);
+        printf("SIMULACAO DO ENVIO DE PACOTES EM UMA REDE DO TIPO \n");
+        printf("CLIENTE/SERVIDOR UTILIZANDO A ESTRUTURA DE DADOS FILA \n\n\n");
+        processar_similacao(fila);
+    }
+
+    for(int i = 0; i < NUMERO_FILA; i++)
+    {
+        TAD_LiberarFila(fila[i]);
+    }
 
     return 0;
 }
