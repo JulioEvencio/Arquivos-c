288f04eef7788aa9fdd6d8c631f99dda46c57bea
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-17 13:52:50 -0300

    Estruturas e constantes de erros foram adicionadas

--- Filas/Fila_Dinamica/fila.h
+++ Filas/Fila_Dinamica/fila.h
@@ -1,6 +1,24 @@
 #ifndef FILA_H_INCLUDED
 #define FILA_H_INCLUDED
 
-//  Code
+#define FILA_SUCESSO 0
+#define FILA_ENDERECO_INVALIDO -1
+#define FILA_FILA_CHEIA -2
+#define FILA_FILA_VAZIA -3
+#define FILA_POSICAO_INEXISTENTE -4
+
+typedef struct Tipo_Nodo Nodo;
+struct Tipo_Nodo
+{
+    Elemento elemento;
+    Nodo *prox;
+};
+
+struct Tipo_Fila
+{
+    Nodo *inicio;
+    Nodo *fim;
+};
+typedef struct Tipo_Fila Fila;
 
 #endif
