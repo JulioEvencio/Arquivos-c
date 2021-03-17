79509173acc71159cadc921d692f273235411a60
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-16 22:20:24 -0300

    Estruturas para Nodo e pilha foram adicionadas

--- Pilhas/Pilha_Dinamica/pilha.h
+++ Pilhas/Pilha_Dinamica/pilha.h
@@ -4,6 +4,19 @@
 /*  Constantes */
 
 /*  Estruturas */
+typedef struct Tipoo_Nodo Nodo;
+struct Tipo_Nodo
+{
+    Elemento elemento;
+    Nodo *prox;
+};
+
+struct Tipo_Pilha
+{
+    int tamanho;
+    Nodo *inicio;
+};
+typedef struct Tipo_Pilha Pilha;
 
 /*  Funcoes */
 
