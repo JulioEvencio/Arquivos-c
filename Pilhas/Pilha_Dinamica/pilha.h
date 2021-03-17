6f624a94cfe748c6d013f72f65ed2817dcd5df57
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-16 22:23:57 -0300

    Constantes e função para inicializar pilha foram adicionadas

--- Pilhas/Pilha_Dinamica/pilha.h
+++ Pilhas/Pilha_Dinamica/pilha.h
@@ -2,6 +2,10 @@
 #define PILHA_H_INCLUDED
 
 /*  Constantes */
+#define PILHA_SUCESSO 0
+#define PILHA_ENDERECO_INVALIDO -1
+#define PILHA_PILHA_CHEIA -2
+#define PILHA_POSICAO_INEXISTENTE -3
 
 /*  Estruturas */
 typedef struct Tipoo_Nodo Nodo;
@@ -19,5 +23,14 @@ struct Tipo_Pilha
 typedef struct Tipo_Pilha Pilha;
 
 /*  Funcoes */
+int FILA_InicialiarPilha(Pilha *pilha)
+{
+    if(pilha == NULL) return PILHA_ENDERECO_INVALIDO;
+    pilha->inicio = NULL;
+    pilha->tamanho = 0;
+    return PILHA_SUCESSO;
+}
+
+
 
 #endif
