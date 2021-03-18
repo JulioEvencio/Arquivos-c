94d633873f8c067722afbcc9c97570182a884419
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-18 12:47:35 -0300

    Novo arquivo para lista estática foi adicionado, constantes também foram adicionadas

---
+++ Listas/Lista_Estatica/lista.h
@@ -0,0 +1,16 @@
+#ifndef LISTA_H_INCLUDED
+#define LISTA_H_INCLUDED
+
+/*  Constantes */
+#define LISTA_TAMANHO_MAX 100
+#define LISTA_SUCESSO 0
+#define LISTA_ENDERECO_INVALIDO -1
+#define LISTA_LISTA_CHEIA -2
+#define LISTA_LISTA_VAZIA -3
+#define LISTA_POSICAO_INEXISTENTE -4
+
+/*  Estruturas */
+
+/*  Funcoes */
+
+#endif
