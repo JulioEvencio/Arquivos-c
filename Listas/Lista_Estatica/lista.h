7278d5552e98ed99201b7d6d343834e190be7b9a
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-18 12:49:16 -0300

    estrutura da lista foi adicionada

--- Listas/Lista_Estatica/lista.h
+++ Listas/Lista_Estatica/lista.h
@@ -2,7 +2,6 @@
 #define LISTA_H_INCLUDED
 
 /*  Constantes */
-#define LISTA_TAMANHO_MAX 100
 #define LISTA_SUCESSO 0
 #define LISTA_ENDERECO_INVALIDO -1
 #define LISTA_LISTA_CHEIA -2
@@ -10,6 +9,13 @@
 #define LISTA_POSICAO_INEXISTENTE -4
 
 /*  Estruturas */
+struct Tipo_Lista
+{
+    int tamanho;
+    int tamanho_max;
+    Elemento *elemento;
+};
+typedef struct Tipo_Lista Lista;
 
 /*  Funcoes */
 
