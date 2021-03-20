3542b26aef48a3a9c5352e6e16dcba815e38dbae
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-20 14:29:54 -0300

    Função que inicializa uma pilha foi adicionada

--- Pilhas/Pilha_Estatica/pilha.h
+++ Pilhas/Pilha_Estatica/pilha.h
@@ -4,7 +4,7 @@
 /*  Constantes */
 #define PILHA_SUCESSO 0
 #define PILHA_ENDERECO_INVALIDO -1
-#define PILHA_LISTA_CHEIA -2
+#define PILHA_CHEIA -2
 #define PILHA_VAZIA -3
 #define PILHA_POSICAO_INEXISTENTE -4
 #define PILHA_TAMANHO_INVALIDO -5
@@ -19,5 +19,15 @@ struct Tipo_Pilha
 typedef struct Tipo_Pilha Pilha;
 
 /*  Funcoes*/
+int PILHA_InicializarPilha(Pilha *pilha, int tamanho)
+{
+    if(pilha == NULL) return PILHA_ENDERECO_INVALIDO;
+    if(tamanho < 1) return PILHA_TAMANHO_INVALIDO;
+    pilha->tamanho = 0;
+    pilha->tamanho_max = tamanho;
+    pilha->elemento = malloc(sizeof(pilha->elemento) * pilha->tamanho_max);
+    if(pilha->elemento == NULL) return PILHA_CHEIA;
+    return PILHA_SUCESSO;
+}
 
 #endif
