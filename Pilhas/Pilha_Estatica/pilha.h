bdeca11686bd513bc8c8440b2f7b5f74b1edc7dd
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-20 14:23:35 -0300

    Constantes para pilha estática foram adicionadas

--- Pilhas/Pilha_Estatica/pilha.h
+++ Pilhas/Pilha_Estatica/pilha.h
@@ -2,6 +2,12 @@
 #define PILHA_H_INCLUDED
 
 /*  Constantes */
+#define PILHA_SUCESSO 0
+#define PILHA_ENDERECO_INVALIDO -1
+#define PILHA_LISTA_CHEIA -2
+#define PILHA_VAZIA -3
+#define PILHA_POSICAO_INEXISTENTE -4
+#define PILHA_TAMANHO_INVALIDO -5
 
 /*  Estruturas */
 
