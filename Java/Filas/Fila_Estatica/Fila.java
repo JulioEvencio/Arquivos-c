8a24470550ac2d38963753e93b3256cd76585f34
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-22 19:25:04 -0300

    Método construtor foi adicionado

--- Java/Filas/Fila_Estatica/Fila.java
+++ Java/Filas/Fila_Estatica/Fila.java
@@ -1,5 +1,13 @@
 public class Fila {
-    public static void main(String[] args) {
-        //  Code
+    private final int[] elemento;
+    private int inicio;
+    private int fim;
+    private int tamanho;
+
+    public Fila(int tamanho) {
+        this.elemento = new int[tamanho];
+        this.inicio = 0;
+        this.fim = 0;
+        this.tamanho = 0;
     }
 }
