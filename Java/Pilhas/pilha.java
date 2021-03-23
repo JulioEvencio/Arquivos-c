9c133597c571723d4e8c5eecd888603a044ba3c1
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-03-23 02:22:08 -0300

    Método construtor adicoinado

--- Java/Pilhas/pilha.java
+++ Java/Pilhas/pilha.java
@@ -1,3 +1,9 @@
 public class Pilha {
-    //  Code
+    private final int[] elemento;
+    private int tamanho;
+
+    public Pilha(int tamanho) {
+        this.elemento = new int[tamanho];
+        this.tamanho = 0;
+    }
 }
