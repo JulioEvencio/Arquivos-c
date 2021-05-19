468a1237f52e5e0e0c3f2656dede26121d042c78
Júlio Evêncio <julioevencio@acad.ifma.edu.br>
2021-05-19 00:08:36 -0300

    Correções

--- C/Lista/Lista Estática/lista.c
+++ C/Lista/Lista Estática/lista.c
@@ -1,24 +1,25 @@
 #include <stdlib.h>
 #include "lista.h"
 
-#define LISTA_INICIO 1
-
-struct Lista {
+struct Lista
+{
     int tamanho;
     int tamanho_max;
     Elemento *elemento;
 };
 
-int lista_inicializar(Lista **lista, int tamanho) {
-    if (tamanho < LISTA_INICIO) return LISTA_TAMANHO_INVALIDO;
-    
+int lista_inicializar(Lista **lista, int tamanho)
+{
+    if (tamanho < 1) return LISTA_TAMANHO_INVALIDO;
+
     *lista = malloc(sizeof **lista);
 
     if (*lista == NULL) return LISTA_SEM_MEMORIA;
 
     (*lista)->elemento = malloc(sizeof((*lista)->elemento) * tamanho);
 
-    if ((*lista)->elemento == NULL) {
+    if ((*lista)->elemento == NULL)
+    {
         free(*lista);
         return LISTA_SEM_MEMORIA;
     }
@@ -29,27 +30,33 @@ int lista_inicializar(Lista **lista, int tamanho) {
     return LISTA_SUCESSO;
 }
 
-void lista_liberar(Lista **lista) {
+void lista_liberar(Lista **lista)
+{
     free((*lista)->elemento);
     free(*lista);
 }
 
-int lista_tamanho(Lista **lista) {
+int lista_obter_tamanho(Lista **lista)
+{
     return (*lista)->tamanho;
 }
 
-int lista_vazia(Lista **lista) {
+int lista_vazia(Lista **lista)
+{
     return (*lista)->tamanho == 0;
 }
 
-int lista_cheia(Lista **lista) {
+int lista_cheia(Lista **lista)
+{
     return (*lista)->tamanho == (*lista)->tamanho_max;
 }
 
-int lista_adicionar_inicio(Lista **lista, Elemento *elemento) {
+int lista_adicionar_inicio(Lista **lista, Elemento *elemento)
+{
     if (lista_cheia(lista)) return LISTA_CHEIA;
 
-    for (int i = (*lista)->tamanho; i >= LISTA_INICIO; i--) {
+    for (int i = (*lista)->tamanho; i >= 1; i--)
+    {
         (*lista)->elemento[i] = (*lista)->elemento[i - 1];
     }
 
@@ -59,12 +66,14 @@ int lista_adicionar_inicio(Lista **lista, Elemento *elemento) {
     return LISTA_SUCESSO;
 }
 
-int lista_adicionar_posicao(Lista **lista, Elemento *elemento, int posicao) {
+int lista_adicionar_posicao(Lista **lista, Elemento *elemento, int posicao)
+{
     if (lista_cheia(lista)) return LISTA_CHEIA;
 
-    if (posicao < LISTA_INICIO || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
+    if (posicao < 1 || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
 
-    for (int i = (*lista)->tamanho; i > (posicao - 1); i--) {
+    for (int i = (*lista)->tamanho; i > (posicao - 1); i--)
+    {
         (*lista)->elemento[i] = (*lista)->elemento[i - 1];
     }
 
@@ -74,7 +83,8 @@ int lista_adicionar_posicao(Lista **lista, Elemento *elemento, int posicao) {
     return LISTA_SUCESSO;
 }
 
-int lista_adicionar_final(Lista **lista, Elemento *elemento) {
+int lista_adicionar_final(Lista **lista, Elemento *elemento)
+{
     if (lista_cheia(lista)) return LISTA_CHEIA;
 
     (*lista)->elemento[(*lista)->tamanho] = *elemento;
@@ -83,10 +93,12 @@ int lista_adicionar_final(Lista **lista, Elemento *elemento) {
     return LISTA_SUCESSO;
 }
 
-int lista_remover_inicio(Lista **lista) {
+int lista_remover_inicio(Lista **lista, Elemento *elemento)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
-    for (int i = 0; i < (*lista)->tamanho; i++) {
+    for (int i = 0; i < (*lista)->tamanho; i++)
+    {
         (*lista)->elemento[i] = (*lista)->elemento[i + 1];
     }
 
@@ -95,12 +107,14 @@ int lista_remover_inicio(Lista **lista) {
     return LISTA_SUCESSO;
 }
 
-int lista_remover_posicao(Lista **lista, int posicao) {
+int lista_remover_posicao(Lista **lista, Elemento *elemento, int posicao)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
-    if (posicao < LISTA_INICIO || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
+    if (posicao < 1 || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
 
-    for (int i = posicao; i < (*lista)->tamanho; i++) {
+    for (int i = posicao; i < (*lista)->tamanho; i++)
+    {
         (*lista)->elemento[i] = (*lista)->elemento[i + 1];
     }
 
@@ -109,7 +123,8 @@ int lista_remover_posicao(Lista **lista, int posicao) {
     return LISTA_SUCESSO;
 }
 
-int lista_remover_final(Lista **lista) {
+int lista_remover_final(Lista **lista, Elemento *elemento)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
     (*lista)->tamanho--;
@@ -117,7 +132,8 @@ int lista_remover_final(Lista **lista) {
     return LISTA_SUCESSO;
 }
 
-int lista_alterar_inicio(Lista **lista, Elemento *elemento) {
+int lista_alterar_inicio(Lista **lista, Elemento *elemento)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
     (*lista)->elemento[0] = *elemento;
@@ -125,17 +141,19 @@ int lista_alterar_inicio(Lista **lista, Elemento *elemento) {
     return LISTA_SUCESSO;
 }
 
-int lista_alterar_posicao(Lista **lista, Elemento *elemento, int posicao) {
+int lista_alterar_posicao(Lista **lista, Elemento *elemento, int posicao)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
-    if (posicao < LISTA_INICIO || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
+    if (posicao < 1 || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
 
     (*lista)->elemento[posicao - 1] = *elemento;
 
     return LISTA_SUCESSO;
 }
 
-int lista_alterar_final(Lista **lista, Elemento *elemento) {
+int lista_alterar_final(Lista **lista, Elemento *elemento)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
     (*lista)->elemento[(*lista)->tamanho - 1] = *elemento;
@@ -143,7 +161,8 @@ int lista_alterar_final(Lista **lista, Elemento *elemento) {
     return LISTA_SUCESSO;
 }
 
-int lista_obter_inicio(Lista **lista, Elemento *elemento) {
+int lista_obter_inicio(Lista **lista, Elemento *elemento)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
     *elemento = (*lista)->elemento[0];
@@ -151,17 +170,19 @@ int lista_obter_inicio(Lista **lista, Elemento *elemento) {
     return LISTA_SUCESSO;
 }
 
-int lista_obter_posicao(Lista **lista, Elemento *elemento, int posicao) {
+int lista_obter_posicao(Lista **lista, Elemento *elemento, int posicao)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
-    if (posicao < LISTA_INICIO || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
+    if (posicao < 1 || posicao > (*lista)->tamanho) return LISTA_POSICAO_INEXISTENTE;
 
     *elemento = (*lista)->elemento[posicao - 1];
 
     return LISTA_SUCESSO;
 }
 
-int lista_obter_final(Lista **lista, Elemento *elemento) {
+int lista_obter_final(Lista **lista, Elemento *elemento)
+{
     if (lista_vazia(lista)) return LISTA_VAZIA;
 
     *elemento = (*lista)->elemento[(*lista)->tamanho - 1];
--- C/Lista/Lista Estática/lista.h
+++ C/Lista/Lista Estática/lista.h
@@ -1,14 +1,18 @@
 #ifndef LISTA_H_INCLUDED
 #define LISTA_H_INCLUDED
 
-#define LISTA_SUCESSO 0
-#define LISTA_SEM_MEMORIA -1
-#define LISTA_VAZIA -2
-#define LISTA_CHEIA -3
-#define LISTA_POSICAO_INEXISTENTE -4
-#define LISTA_TAMANHO_INVALIDO -5
+enum
+{
+    LISTA_SUCESSO,
+    LISTA_SEM_MEMORIA,
+    LISTA_VAZIA,
+    LISTA_CHEIA,
+    LISTA_POSICAO_INEXISTENTE,
+    LISTA_TAMANHO_INVALIDO
+};
 
-struct Elemento {
+struct Elemento
+{
     int x;
 };
 typedef struct Elemento Elemento;
@@ -18,7 +22,7 @@ typedef struct Lista Lista;
 int lista_inicializar(Lista **lista, int tamanho);
 void lista_liberar(Lista **lista);
 
-int lista_tamanho(Lista **lista);
+int lista_obter_tamanho(Lista **lista);
 int lista_vazia(Lista **lista);
 int lista_cheia(Lista **lista);
 
@@ -26,9 +30,9 @@ int lista_adicionar_inicio(Lista **lista, Elemento *elemento);
 int lista_adicionar_posicao(Lista **lista, Elemento *elemento, int posicao);
 int lista_adicionar_final(Lista **lista, Elemento *elemento);
 
-int lista_remover_inicio(Lista **lista);
-int lista_remover_posicao(Lista **lista, int posicao);
-int lista_remover_final(Lista **lista);
+int lista_remover_inicio(Lista **lista, Elemento *elemento);
+int lista_remover_posicao(Lista **lista, Elemento *elemento, int posicao);
+int lista_remover_final(Lista **lista, Elemento *elemento);
 
 int lista_alterar_inicio(Lista **lista, Elemento *elemento);
 int lista_alterar_posicao(Lista **lista, Elemento *elemento, int posicao);
