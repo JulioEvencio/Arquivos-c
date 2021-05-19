dc720a1cd9bca8c33ececbaa5bbb02e72bebf97b
Júlio Evêncio <julioevencio@acad.ifma.edu.br>
2021-05-19 16:30:25 -0300

    Árvore Binária adicionada

---
+++ C/arvore/arvore_binaria/arvore.c
@@ -0,0 +1,158 @@
+#include <stdlib.h>
+#include "arvore.h"
+
+typedef struct Arvore Nodo;
+struct Arvore
+{
+    int chave;
+    Elemento elemento;
+    Nodo *esquerda;
+    Nodo *direita;
+};
+
+Nodo *arvore_localizar_nodo(Arvore **arvore, int chave);
+
+void arvore_inicializar(Arvore **arvore)
+{
+    *arvore = NULL;
+}
+
+void arvore_liberar(Arvore **arvore)
+{
+    if (*arvore == NULL) return;
+
+    arvore_liberar(&(*arvore)->esquerda);
+    arvore_liberar(&(*arvore)->direita);
+
+    free(*arvore);
+}
+
+int arvore_obter_quantidade_nodo(Arvore **arvore)
+{
+    int tamanho = 0;
+
+    if (*arvore != NULL)
+    {
+        tamanho++;
+        tamanho += arvore_obter_quantidade_nodo(&(*arvore)->esquerda);
+        tamanho += arvore_obter_quantidade_nodo(&(*arvore)->direita);
+    }
+
+    return tamanho;
+}
+
+int arvore_vazia(Arvore **arvore)
+{
+    return *arvore == NULL;
+}
+
+Nodo *arvore_localizar_nodo(Arvore **arvore, int chave)
+{
+    Nodo *esquerda = NULL, *direita = NULL;
+
+    if ((*arvore)->chave == chave) return *arvore;
+
+    if ((*arvore)->esquerda != NULL)
+    {
+        esquerda = arvore_localizar_nodo(&(*arvore)->esquerda, chave);
+    }
+
+    if (esquerda == NULL)
+    {
+        if ((*arvore)->direita != NULL)
+        {
+            direita = arvore_localizar_nodo(&(*arvore)->direita, chave);
+        }
+        return direita;
+    }
+
+    return esquerda;
+}
+
+int arvore_criar_raiz(Arvore **arvore, Elemento *elemento, int chave)
+{
+    if (*arvore != NULL) return ARVORE_NODO_OCUPADO;
+
+    *arvore = malloc(sizeof **arvore);
+
+    if (*arvore == NULL) return ARVORE_SEM_MEMORIA;
+
+    (*arvore)->chave = chave;
+    (*arvore)->elemento = *elemento;
+    (*arvore)->esquerda = NULL;
+    (*arvore)->direita = NULL;
+
+    return ARVORE_SUCESSO;
+}
+
+int arvore_adicionar_esquerda(Arvore **arvore, int chave_pai, Elemento *elemento, int chave_filho)
+{
+    Nodo *pai = NULL, *filho = NULL;
+
+    if (*arvore == NULL) return ARVORE_NULA;
+
+    pai = arvore_localizar_nodo(arvore, chave_pai);
+
+    if (pai == NULL) return ARVORE_CHAVE_INVALIDA;
+
+    if (pai->esquerda != NULL) return ARVORE_NODO_OCUPADO;
+
+    filho = malloc(sizeof *filho);
+
+    if (filho == NULL) return ARVORE_SEM_MEMORIA;
+
+    filho->chave = chave_filho;
+    filho->elemento = *elemento;
+    filho->esquerda = NULL;
+    filho->direita = NULL;
+    pai->esquerda = filho;
+
+    return ARVORE_SUCESSO;
+}
+
+int arvore_adicionar_direita(Arvore **arvore, int chave_pai, Elemento *elemento, int chave_filho)
+{
+    Nodo *pai = NULL, *filho = NULL;
+
+    if (*arvore == NULL) return ARVORE_NULA;
+
+    pai = arvore_localizar_nodo(arvore, chave_pai);
+
+    if (pai == NULL) return ARVORE_CHAVE_INVALIDA;
+
+    if (pai->direita != NULL) return ARVORE_NODO_OCUPADO;
+
+    filho = malloc(sizeof *filho);
+
+    if (filho == NULL) return ARVORE_SEM_MEMORIA;
+
+    filho->chave = chave_filho;
+    filho->elemento = *elemento;
+    filho->esquerda = NULL;
+    filho->direita = NULL;
+    pai->direita = filho;
+
+    return ARVORE_SUCESSO;
+}
+
+int arvore_alterar(Arvore **arvore, Elemento *elemento, int chave)
+{
+    Nodo *nodo = arvore_localizar_nodo(arvore, chave);
+
+    if (nodo == NULL) return ARVORE_CHAVE_INVALIDA;
+
+    nodo->elemento = *elemento;
+
+    return ARVORE_SUCESSO;
+}
+
+int arvore_obter(Arvore **arvore, Elemento *elemento, int chave)
+{
+    Nodo *nodo = arvore_localizar_nodo(arvore, chave);
+
+    if (nodo == NULL) return ARVORE_CHAVE_INVALIDA;
+
+    *elemento = nodo->elemento;
+
+    return ARVORE_SUCESSO;
+}
---
+++ C/arvore/arvore_binaria/arvore.h
@@ -0,0 +1,34 @@
+#ifndef ARVORE_H_INCLUDED
+#define ARVORE_H_INCLUDED
+
+enum
+{
+    ARVORE_SUCESSO,
+    ARVORE_NULA,
+    ARVORE_SEM_MEMORIA,
+    ARVORE_CHAVE_INVALIDA,
+    ARVORE_NODO_OCUPADO
+};
+
+struct Elemento
+{
+    int x;
+};
+typedef struct Elemento Elemento;
+
+typedef struct Arvore Arvore;
+
+void arvore_inicializar(Arvore **arvore);
+void arvore_liberar(Arvore **arvore);
+
+int arvore_obter_quantidade_nodo(Arvore **arvore);
+int arvore_vazia(Arvore **arvore);
+
+int arvore_criar_raiz(Arvore **arvore, Elemento *elemento, int chave);
+int arvore_adicionar_esquerda(Arvore **arvore, int chave_pai, Elemento *elemento, int chave_filho);
+int arvore_adicionar_direita(Arvore **arvore, int chave_pai, Elemento *elemento, int chave_filho);
+
+int arvore_alterar(Arvore **arvore, Elemento *elemento, int chave);
+int arvore_obter(Arvore **arvore, Elemento *elemento, int chave);
+
+#endif
